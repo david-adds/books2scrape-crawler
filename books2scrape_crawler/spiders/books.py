ed502fa2972f00e9171e35b0a5a15f8e1f396eef
david-adds <55636118+david-adds@users.noreply.github.com>
2021-12-28 23:41:02 -0300

    scrape book name, price, availability, genre and url

--- books2scrape_crawler/spiders/books.py
+++ books2scrape_crawler/spiders/books.py
@@ -6,15 +6,18 @@ from scrapy.spiders import CrawlSpider, Rule
 class BooksSpider(CrawlSpider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
+    start_urls = ['http://books.toscrape.com']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//ol[@class='row']//h3/a"), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//ul[@class='pager']/li[position()=last()]/a"))
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield{
+            'name': response.xpath("//article[@class='product_page']//h1/text()").get(),
+            'price': response.xpath("//p[@class='price_color']/text()").get(),
+            'availability': response.xpath("normalize-space((//p[@class='instock availability']/text())[2])").get(),
+            'genre': response.xpath(".//ul[@class='breadcrumb']/li[3]/a/text()").get(),
+            'book_url': response.url
+        }
